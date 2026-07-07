5bc0b073363e85046577ca0f4dc1917f47e13847
Emanoel011 <163313225+Emanoel011@users.noreply.github.com>
2026-07-07 13:01:32 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // SUBSTUIA 'nome-do-seu-repositorio' pelo nome exato do projeto no GitHub
-  base: '/rede-de-cuidado/', 
+  base: '/rede-de-cuidado/', // Deve ser exatamente o nome do seu repositório no GitHub
 })
