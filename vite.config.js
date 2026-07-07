003920f0d141916352e2cc48d9227dee41f2b960
Emanoel011 <163313225+Emanoel011@users.noreply.github.com>
2026-07-07 13:13:29 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rede-de-cuidado/', // Deve ser exatamente o nome do seu repositório no GitHub
+  base: './', // Caminho relativo universal: resolve 99% dos problemas de tela branca no GitHub Pages
 })
