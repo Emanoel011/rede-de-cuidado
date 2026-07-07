a1ecfd5f86dd993fee91f332e26d370b2cebf523
Emanoel011 <163313225+Emanoel011@users.noreply.github.com>
2026-07-07 13:24:57 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/rede-de-cuidado/', // Caminho exato do seu repositório no GitHub
-})
